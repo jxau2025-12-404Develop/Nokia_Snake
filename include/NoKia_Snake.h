9cf303ac72e27f48694192fbef7c5ad4443d97e8
jyoushitou <xzt20060906@outlook.com>
2026-09-06 12:58:45 +0800

    fix：更新成对应的

--- include/NoKia_Snake.h
+++ include/NoKia_Snake.h
@@ -1,15 +1,5 @@
 // Nokia_Snake.h
 #pragma once
 
-#include "Renderer.h"
-
-// 重置画面
-// 将所有数值重置为开始
-void InitGV(GameView& GV);
-
-// 游戏主程序
-// 无参
-int Game(GameView& GV);
-
 // 程序运行的主函数
 void Nokia_Snake();
